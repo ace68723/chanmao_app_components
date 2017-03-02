14b7e064125b752538f1515815bac24477e8ce5c
AKACC <hi_jackou@hotmail.com>
2017-03-02 18:42:33 -0500

    add abouus component

---
+++ index.ios.js
@@ -0,0 +1,53 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  NavigatorIOS,
+} from 'react-native';
+import InfoPage from './intro/infoPage'
+export default class cmInfo extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+
+        style={{
+          flex:1
+        }}
+        initialRoute={{
+          component:InfoPage,
+          title:'关于我们',
+
+        }}
+        />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f2f2f2',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('cmInfo', () => cmInfo);
